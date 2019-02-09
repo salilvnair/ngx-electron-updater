9058db588e94536dc497ca008c9a153c329eabb2
salilvnair <salilvnair@gmail.com>
2019-02-10 04:27:46 +0530

    added delete functionality for both empty tag and draft release, need to check for released tag

--- cli/cmds/publish.js
+++ cli/cmds/publish.js
@@ -10,6 +10,22 @@ module.exports = async (args, draft) => {
             process.exit();
         }
         let appName = args._[1];
+        //console.log(args)
+        if(args.d||args.delete||args.D){
+           if(args.t||args.tag) {
+            tag = args.t || args.tag;
+            await processDeletePublishedVersion(appName,tag,args);
+            process.exit();
+           } 
+           else if(args.emptyTags||args.etgs){
+            await processDeleteEmptyTags(appName,args);
+            process.exit();
+           }
+           else{
+            console.log(chalk.red('\nError:Tag name is not specified to be deleted, please specify using command:')+' ngxeu publish MyApp'+chalk.cyan(' -d -t v1.0.0'));
+            process.exit();
+           }
+        }
         if(args.f || args.file) {
             let fileArgs = args.f || args.file;
             files = [];
@@ -17,7 +33,7 @@ module.exports = async (args, draft) => {
             //console.log(files);
         }
         else{
-            console.log(chalk.red('\nError:file(s) path not mentioned please pass using command:')+' ngxeu publish'+chalk.cyan(' -f ./test.zip'));
+            console.log(chalk.red('\nError:file(s) path not mentioned please pass using command:')+' ngxeu publish MyApp'+chalk.cyan(' -f ./test.zip'));
             process.exit();
         }
         if(args.t || args.tag) {
@@ -25,7 +41,7 @@ module.exports = async (args, draft) => {
             //console.log(tag);
         }
         else{
-            console.log(chalk.red('\nError:release/draft tag name is not mentioned please pass using command:')+' ngxeu publish'+chalk.cyan(' -t v1.0.0'));
+            console.log(chalk.red('\nError:release/draft tag name is not mentioned please pass using command:')+' ngxeu publish MyApp'+chalk.cyan(' -t v1.0.0'));
             process.exit();
         }
         draft = true;
@@ -65,3 +81,93 @@ module.exports = async (args, draft) => {
         files.push('./app-release.json');        
         ghRelease.uploadAsset(accessToken,repoDetails.user,repoDetails.repo,name,tag,target,notes,files,draft);
   }
+
+  async function processDeletePublishedVersion(appName,tag,args){
+    let accessToken = github.getStoredGithubToken(appName);       
+    if(!accessToken) {
+       console.log(chalk.red('\nError:access token not found please set one using command: ')+chalk.cyan('ngxeu init '+appName));
+       process.exit();
+    }
+    let repoDetails = github.getStoredRepoDetails(appName);
+    if(!repoDetails){
+        console.log(chalk.red('\nError:repository details not found please set one using command: ')+chalk.cyan('ngxeu init '+appName));
+        process.exit();
+    }
+    listCmd="all";
+    let releases = await ghRelease.listRelease(accessToken,repoDetails.user,repoDetails.repo,listCmd);
+    let hasTag = validateReleasesByTag(releases,tag);
+    if(hasTag){
+        if(hasMultipleEntriesForTag(releases,tag)){
+            if(args.D){
+                let releaseIds = getReleaseIds(releases,tag);
+                await ghRelease.deleteReleaseByTag(accessToken,repoDetails.user,repoDetails.repo,tag,releaseIds,null)
+            }
+            else{
+                console.log(chalk.red('\nError:Multiple entries with tag name ')+chalk.cyan(tag)+' found.Please use '+chalk.cyan('-D')+' to force delete all. eg:'+chalk.cyan('ngxeu publish MyApp -D -t v1.0.0'));
+                process.exit(); 
+            }
+        }
+        else{
+            let releaseIds = getReleaseIds(releases,tag);
+            await ghRelease.deleteReleaseByTag(accessToken,repoDetails.user,repoDetails.repo,tag,releaseIds,null)
+        }  
+    }
+    else{
+        console.log(chalk.red('\nError('+chalk.cyan('404')+'): No tag name with ')+chalk.cyan(tag)+chalk.red(' found.'));
+        process.exit();
+    }
+  }
+
+  async function processDeleteEmptyTags(appName,args){
+    let tagsArgs = args.etgs || args.emptyTags;
+    let emptyTags = [];
+    emptyTags = tagsArgs.split(",");
+    let accessToken = github.getStoredGithubToken(appName);       
+    if(!accessToken) {
+       console.log(chalk.red('\nError:access token not found please set one using command: ')+chalk.cyan('ngxeu init '+appName));
+       process.exit();
+    }
+    let repoDetails = github.getStoredRepoDetails(appName);
+    if(!repoDetails){
+        console.log(chalk.red('\nError:repository details not found please set one using command: ')+chalk.cyan('ngxeu init '+appName));
+        process.exit();
+    }
+    listCmd="all";
+    await ghRelease.deleteReleaseByTag(accessToken,repoDetails.user,repoDetails.repo,null,null,emptyTags);
+    process.exit();
+  }
+
+  function validateReleasesByTag(releases,tag) {
+      let hasTag = false;
+      releases.forEach(element => {
+          if(element.tag_name===tag){
+            hasTag = true;
+          }
+      });
+      return hasTag;
+  }
+
+  function getReleaseIds(releases,tag){
+    let releaseIds = [];
+    releases.forEach(element => {
+        if(element.tag_name===tag){
+            releaseIds.push(element.id);
+        }
+    });
+    return releaseIds;
+  }
+
+  function hasMultipleEntriesForTag(releases,tag) {
+    let hasMulitpleEntries = false;
+    let counter = 0;
+    releases.forEach(element => {
+        if(element.tag_name===tag){
+          counter++;
+        }
+    });
+    if(counter>1){
+        hasMulitpleEntries = true;
+    }
+    return hasMulitpleEntries;
+  }
+
--- cli/util/gh-release.util.js
+++ cli/util/gh-release.util.js
@@ -1,10 +1,12 @@
 const chalk = require('chalk');
 const Octokit = require('@octokit/rest');
 const octokit = new Octokit();
-var prettyBytes = require('pretty-bytes');
-const cliSpinners = require('cli-spinners');
+var util = require('util')
 var log = require('single-line-log').stdout;
+var pkg = require('../package.json')
 const GithubPublishUtil = require('../util/github-publish.util');
+var rp = require('request-promise');
+var GITHUB_API_ROOT = 'https://api.github.com';
 module.exports = {
     listRelease: async(ghToken,owner, repo, listCmd) => {
         try {
@@ -48,6 +50,7 @@ module.exports = {
             console.log(newLineToken+'Successfully published at: ' + release.html_url)
             process.exit(0);
         });
+        githubPublishUtil.publish();
         githubPublishUtil.on('asset-info', function (name,size) {
             createBars(name,size);
         });
@@ -58,7 +61,27 @@ module.exports = {
             log.clear();
         });
 
+    },
+
+    deleteReleaseByTag:async(ghToken,owner, repo,tag,releaseIds,emptyTags) => {
+        let options = {};
+        options.owner = owner;
+        options.tag = tag;
+        options.repo = repo;
+        options.token = ghToken;
+        options.emptyTags = emptyTags;
+        if(emptyTags){
+            for(i=0;i<emptyTags.length;i++){
+                await deleteReleaseId(emptyTags[i],options);
+            }
+        }
+        else{
+            for(i=0;i<releaseIds.length;i++){
+                await deleteReleaseId(releaseIds[i],options);
+            }
+        }
     }
+
     
 };
 var multiprog = require("./progressbar.util");
@@ -87,3 +110,32 @@ function validateGHToken(ghToken) {
         token: ghToken
     });
 }
+
+async function deleteReleaseId(id,opts){
+    var deleteTagUri;
+    if(opts.emptyTags){
+        deleteTagUri = util.format((opts.apiUrl || GITHUB_API_ROOT) + '/repos/%s/%s/git/refs/tags/%s', opts.owner, opts.repo, id)
+    }
+    else{
+        deleteTagUri = util.format((opts.apiUrl || GITHUB_API_ROOT) + '/repos/%s/%s/releases/%s', opts.owner, opts.repo, id);
+    }
+    const requestOptions  = {
+        method:'DELETE',
+        uri:deleteTagUri,
+        headers: {
+        'Authorization': 'token ' + opts.token,
+        'user-agent': 'ngxeu ' + pkg.version  
+        },
+        json: true ,
+        resolveWithFullResponse: true 
+        
+    }     
+    await rp(requestOptions)
+        .then(function (response) {
+            console.log(chalk.green("\nSuccess(")+chalk.cyan(response.statusCode)+chalk.green(" ): Tag ")+chalk.cyan(opts.tag)+chalk.green(" deleted successfully!"));
+        })
+        .catch(function (err) {
+            console.log(chalk.red('\nError('+chalk.cyan(err.statusCode)+'): ')+chalk.red(err.error.message+' for tag ')+chalk.cyan(id));
+        });
+  }
+
